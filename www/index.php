b39a5c5f50de17bd8a062b257997d6821e552ed0
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-06-17 17:00:07 +0900

    馬渕git接続テスト

--- www/index.php
+++ www/index.php
@@ -1,7 +1,11 @@
 <?php
 include "lib/connect_db.php";
 
-
+try {
+    $pdo = connect_db();
+} catch (\Throwable $th) {
+    //throw $th;
+}
 
 
 ?>
@@ -14,7 +18,7 @@ include "lib/connect_db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magonote</title>
+    <title>classcommunity</title>
     <link rel="stylesheet" href="./style/style.css">
 </head>
 
