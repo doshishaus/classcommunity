da1cf56fd28d53c7203130d5b8f325eb885ebc0c
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-06-23 19:38:15 +0900

    add mixins_scss

--- www/index.php
+++ www/index.php
@@ -9,7 +9,7 @@ include "./inc/header.php";
         <h1>評論文を書こう</h1>
     </header>
     <main>
-        <div>
+        <div class="links-container">
             <p><a href="form.php">フォーム</a></p>
             <p><a href="all.php">全体コメント</a></p>
         </div>
--- www/wait.php
+++ www/wait.php
@@ -6,10 +6,12 @@ include "./inc/header.php";
 </head>
 
 <body class="wait-benchmark">
-    <div class="wait-button-background">
-        <a href="./all.php">
-            <p>→</p>
-            <p>先生の指示を待って押しましょう</p>
+    <div class="wait-button-layout">
+        <a href="./all.php" class="wait-button-a">
+            <div class="wait-button-color">
+                <p>→</p>
+                <p>先生の指示を待って押しましょう</p>
+            </div>
         </a>
     </div>
 </body>
