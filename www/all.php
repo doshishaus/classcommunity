0178b45b9f4646d93dfcf2af1e55e8b878e7b0a0
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-06-28 22:42:30 +0900

    デザインとりあえず

--- www/all.php
+++ www/all.php
@@ -26,18 +26,23 @@ include "./inc/header.php";
             <h1>全体の意見</h1>
             <p class="title-header-p">議論のテーマはまぶちが設定して</p>
         </div>
-        <?php foreach ($all_stmt as $key => $value) : ?>
-            <details class="accordion-list">
-                <summary><?= htmlspecialchars($array[$value["g_num"] - 1]) ?>班　<?= htmlspecialchars($value["g_comment"]) ?></summary>
-                <div>
-                    <?php foreach ($all_comment as $comment) : ?>
-                        <?php if ($value["g_num"] == $comment["g_num"]) : ?>
-                            <p><?= htmlspecialchars($comment["username"]) ?>：<?= htmlspecialchars($comment["comment"]) ?></p>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
-            </details>
-        <?php endforeach; ?>
+        <div class="accordion-list-top">
+            <?php foreach ($all_stmt as $key => $value) : ?>
+                <details class="accordion-list">
+                    <summary class="accordion-list-summary"><span class="accordion-list-span"><?= htmlspecialchars($array[$value["g_num"] - 1]) ?>班</span>　<?= htmlspecialchars($value["g_comment"]) ?></summary>
+                    <div class="accordion-list-main">
+                        <?php foreach ($all_comment as $comment) : ?>
+                            <?php if ($value["g_num"] == $comment["g_num"]) : ?>
+                                <div class="accordion-list-border">
+                                    <p class="accordion-list-main-name"><?= htmlspecialchars($comment["username"]) ?></p>
+                                    <p class="accordion-list-main-content"><?= htmlspecialchars($comment["comment"]) ?></p>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                </details>
+            <?php endforeach; ?>
+        </div>
     </div>
 </body>
 
